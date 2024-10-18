341908114354e52bbd6b2a91f6f7a39b2cf0bfcf
Albin Edegran <albinedegran@gmail.com>
2024-10-18 15:11:50 +0200

    fixed the dice loss to not use hard dice and to also be a loss ._.

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,7 +6,20 @@ def dice_coefficient(pred, target, smooth=1.):
     intersection = (pred * target).sum()
     return (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
 
+def soft_dice_coefficient(pred, target, smooth=1.):
+    intersection = (pred * target).sum()
+    return (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
+
+def soft_dice_loss(pred, target, smooth=1.):
+    return 1 - soft_dice_coefficient(pred, target, smooth)
+
+def soft_iou(pred, target, smooth=1e-6):
+    intersection = (pred * target).sum()
+    union = pred.sum() + target.sum() - intersection
+    return (intersection + smooth) / (union + smooth)
+
 def iou(pred, target, smooth=1e-6):
+    pred = pred > 0.5  # Thresholding to get binary predictions
     intersection = (pred * target).sum()
     union = pred.sum() + target.sum() - intersection
     return (intersection + smooth) / (union + smooth)
@@ -23,5 +36,5 @@ class CombinedLoss(nn.Module):
 
     def forward(self, outputs, targets):
         ce_loss = self.bce_loss(outputs, targets)
-        dice = dice_coefficient(torch.sigmoid(outputs), targets, self.smooth)
+        dice = soft_dice_loss(torch.sigmoid(outputs), targets, self.smooth)
         return ce_loss + dice
