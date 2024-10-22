6fd5221a7b13d3568685705ef57b66bd299bda7c
Albin Edegran <albinedegran@gmail.com>
2024-10-22 10:34:32 +0200

    optimized code for datasets by precomputing bounding boxes

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -10,10 +10,10 @@ class TrainDataset(Dataset):
     preoad_all_images = True => all images are loaded into memory at the start
     """
 
-    def __init__(self, data_dir, labels_dir, patch_size, require_target=False,transform=None, preload_all_images=True, num_patches_per_image=1):
+    def __init__(self, data_dir, labels_dir, patch_size, require_target=0, transform=None, preload_all_images=True, num_patches_per_image=1):
         self.patch_size = patch_size
         self.transform = transform
-        self.require_target = require_target
+        self.require_target = float(require_target)
         
         self.data_paths = sorted(glob(data_dir + '/*.nii'))
         self.label_paths = sorted(glob(labels_dir + '/*.nii'))
@@ -22,6 +22,7 @@ class TrainDataset(Dataset):
         if preload_all_images:
             self.data = {path_: sitk.GetArrayFromImage(sitk.ReadImage(path_)).astype('float32') for path_ in self.data_paths}
             self.labels = {path_: sitk.GetArrayFromImage(sitk.ReadImage(path_)).astype('int64') for path_ in self.label_paths}
+            self.bounding_boxes = {path_: self.compute_bounding_box(self.labels[path_]) for path_ in self.label_paths}
 
         self.num_patches_per_image = num_patches_per_image
 
@@ -32,18 +33,20 @@ class TrainDataset(Dataset):
         if self.preload_all_images:
             image_np = self.data[self.data_paths[idx]]
             label_np = self.labels[self.label_paths[idx]]
+            bounding_box = self.bounding_boxes[self.label_paths[idx]]
         else:
             image  = sitk.ReadImage(self.data_paths[idx])
             label = sitk.ReadImage(self.label_paths[idx])
 
             image_np = sitk.GetArrayFromImage(image).astype('float32')
             label_np = sitk.GetArrayFromImage(label).astype('int64')
-
+            bounding_box = None
+            
         patches = []
         labels = []
         for _ in range(self.num_patches_per_image):
-            if self.require_target:
-                image_patch, label_patch = self.random_crop_3d_target(image_np, label_np, self.patch_size)
+            if torch.rand(1).item() < self.require_target:
+                image_patch, label_patch = self.random_crop_3d_target(image_np, label_np, self.patch_size, bounding_box)
             else:
                 image_patch, label_patch = self.random_crop_3d(image_np, label_np, self.patch_size)
             
@@ -65,22 +68,30 @@ class TrainDataset(Dataset):
         sx = torch.randint(0, x - px, (1,)).item()
         return image[sz:sz+pz, sy:sy+py, sx:sx+px], label[sz:sz+pz, sy:sy+py, sx:sx+px]
     
-    def random_crop_3d_target(self, image, label, patch_size):
+    def random_crop_3d_target(self, image, label, patch_size, bounding_box=None):
         z, y, x = image.shape
         pz, py, px = patch_size
         
-        ones_indices = np.argwhere(label)
-        z_min, y_min, x_min = ones_indices.min(axis=0)
-        z_max, y_max, x_max = ones_indices.max(axis=0)
+        if bounding_box is None:
+            bounding_box = self.compute_bounding_box(label)
 
-        z_patch_min, y_patch_min, x_patch_min = max(0, z_min - pz), max(0, y_min - py), max(0, x_min - px)
-        z_patch_max, y_patch_max, x_patch_max = min(z - pz, z_max), min(y - py, y_max), min(x - px, x_max)
+        z_patch_min, y_patch_min, x_patch_min, z_patch_max, y_patch_max, x_patch_max = bounding_box
 
         sz = torch.randint(z_patch_min, z_patch_max + 1, (1,)).item()
         sy = torch.randint(y_patch_min, y_patch_max + 1, (1,)).item()
         sx = torch.randint(x_patch_min, x_patch_max + 1, (1,)).item()
 
         return image[sz:sz+pz, sy:sy+py, sx:sx+px], label[sz:sz+pz, sy:sy+py, sx:sx+px]
+
+    def compute_bounding_box(self, label):
+        ones_indices = np.argwhere(label)
+        z_min, y_min, x_min = ones_indices.min(axis=0)
+        z_max, y_max, x_max = ones_indices.max(axis=0)
+
+        z_patch_min, y_patch_min, x_patch_min = max(0, z_min - self.patch_size[0]), max(0, y_min - self.patch_size[1]), max(0, x_min - self.patch_size[2])
+        z_patch_max, y_patch_max, x_patch_max = min(label.shape[0] - self.patch_size[0], z_max), min(label.shape[1] - self.patch_size[1], y_max), min(label.shape[2] - self.patch_size[2], x_max)
+
+        return z_patch_min, y_patch_min, x_patch_min, z_patch_max, y_patch_max, x_patch_max
 
         
     
